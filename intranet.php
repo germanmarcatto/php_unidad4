afe0b6b2847fb7f4346c4e157ec576b16ca1d9cc
germanmarcatto <112911870+germanmarcatto@users.noreply.github.com>
2022-09-05 21:34:38 -0300

    Create intranet.php

---
+++ intranet.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intranet - Solo acceso restringido</title>
+
+    <head>
+        <link rel="stylesheet" href="estilos_web.css">
+    </head>
+
+<body>
+    <h3>No tiene acceso</h3>
+</body>
+
+<footer class = piedepagina >
+        <div class="box"></div>
+        <a href="index.php">Regresar al Inicio </a></li>
+</footer>
+
+</html>
